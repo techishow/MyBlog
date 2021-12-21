0f4fa4e67fd8a7beee92cdfafd48caee4ea9b7b8
TECHISHOW <80113660+techishow@users.noreply.github.com>
2021-12-20 22:21:55 -0800

    Edit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="w-screen h-[50vh] bg-black rounded-b-3xl flex flex-col justify-center px-10 md:px-20 lg:px-40">
       <h1 className="font-mono text-4xl italic font-bold break-all sm:text-6xl 2xl:text-7xl">
-        function()
+        Amsi()
       </h1>
       <p className="mt-5 sm:text-xl md:text-2xl">
         <InlineLink link="https://github.com/techishow" redirect={true}>
