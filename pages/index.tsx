3bef2a6b39b47039a0324311c895766c7ec6fe01
TECHISHOW <80113660+techishow@users.noreply.github.com>
2021-12-23 06:30:24 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,3 +29,10 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 export default Index;
+
+
+<a title="Real Time Web Analytics" href="http://clicky.com/101347416"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script async src="//static.getclicky.com/101347416.js"></script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101347416ns.gif" /></p></noscript>
+ </body>
+</html>
